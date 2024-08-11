9e69fc1c7953465769a6af6337374b226c42cd81
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-08-10 20:01:31 -0500

    str setter y getter clase futbolista

--- futbolista.py
+++ futbolista.py
@@ -11,4 +11,23 @@ class Futbolista(Persona, Deportista):
         self._piernaHabil = piernaHabil
         Futbolista.lista_futbolistas.append(self)
 
+    def getGolesMarcados(self):
+        return self._golesMarcados
     
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+    
+    def __str__(self):
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
