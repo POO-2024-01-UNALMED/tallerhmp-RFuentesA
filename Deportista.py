a84ffd041cf97f16a5ebd31036bd576d3b82f094
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-08-10 19:22:38 -0500

    Creación de clases y atributos

---
+++ Deportista.py
@@ -0,0 +1,4 @@
+class Deportista:
+    def __init__(self, deporte, anosPracticando):
+        self._deporte = deporte
+        self._anosPracticando = anosPracticando
---
+++ Futbolista.py
@@ -0,0 +1,3 @@
+class Futbolista:
+    def __init__(self, golesMarcados):
+        self._golesMarcados = golesMarcados
