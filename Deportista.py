36e61b05a2c9cfc80a9478a5afa520774ae2615b
RFuentesA <159492816+RFuentesA@users.noreply.github.com>
2024-08-10 19:37:46 -0500

    getter y setter clase deportista

--- Deportista.py
+++ Deportista.py
@@ -3,4 +3,14 @@ class Deportista:
         self._deporte = deporte
         self._añosPracticando = añosPracticando
 
+    def getDeporte(self):
+        return self._deporte
     
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getAñosPracticando(self):
+        return self._añosPracticando
+
+    def setAñosPracticando(self, añosPracticando):
+        self._añosPracticando = añosPracticando
